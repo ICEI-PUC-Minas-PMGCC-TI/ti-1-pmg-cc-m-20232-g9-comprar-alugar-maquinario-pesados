ad4a0d629af91eb4196c40e53f15cd8e0d56df41
igorribb <igorribeirofagundesgdi@gmail.com>
2023-10-23 15:31:43 -0300

    filtro e pesquisaa

---
+++ codigo/sprint2igor.pesquisa/pesquisa.js
@@ -0,0 +1,16 @@
+function filterProducts() {
+    const searchInput = document.getElementById('search');
+    const productList = document.getElementById('product-list');
+    const productItems = productList.getElementsByClassName('product-item');
+    const query = searchInput.value.toLowerCase();
+
+    for (const item of productItems) {
+        const productName = item.textContent.toLowerCase();
+
+        if (productName.includes(query)) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    }
+}
